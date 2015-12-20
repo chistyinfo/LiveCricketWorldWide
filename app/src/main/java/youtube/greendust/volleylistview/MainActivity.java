21a384a0df8a78100313f37039394d5f191d0923
hasan_cse91 <hasan_cse91@yahoo.com>
2015-12-20 22:00:28 +0600

    Dynamic URL added from server

--- app/src/main/java/youtube/greendust/volleylistview/MainActivity.java
+++ app/src/main/java/youtube/greendust/volleylistview/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 	private CustomListAdapter adapter;
 	String[] urlStrArray;
 
-
+//
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
